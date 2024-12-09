655bdf939684ae2ce2b3d82b795a4977f0660ade
adil <adilsaid64@gmail.com>
2024-12-09 20:01:03 +0000

    test: added test for append

--- src/linkedlist/AppTest.java
+++ src/linkedlist/AppTest.java
@@ -1,10 +1,22 @@
 package linkedlist;
 
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
 public class AppTest {
+
+    @Test
+    public void testAppend(){
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.makeEmpty();
+        myLinkedList.append(1);
+        myLinkedList.append(2);
+
+         assertEquals(1, myLinkedList.getHead().value);
+         assertEquals(2, myLinkedList.getTail().value);
+         assertEquals(2, myLinkedList.getLength());
+    }
+
     @Test
     public void testRemoveLast(){
         LinkedList myLinkedList = new LinkedList(1);
